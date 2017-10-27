4c7f1a1e0814b5b3114ef00f286c707c48dd99b5
ikostan <superegorius@gmail.com>
2017-10-26 21:48:24 -0700

    Delegates chaining

--- CustomDelegate/Program.cs
+++ CustomDelegate/Program.cs
@@ -12,13 +12,25 @@ namespace CustomDelegate
 
         static void Main(string[] args)
         {
+            WorkPerformedHandler del = new WorkPerformedHandler(WorkPerformed);
             WorkPerformedHandler workHandler = new WorkPerformedHandler(WorkPerformed);
             WorkPerformedHandler performanceHandler = new WorkPerformedHandler(DoSomeWork);
 
-            workHandler(8, WorkType.GenerateReports);
-            performanceHandler(16, WorkType.Negotiation);
+            //workHandler(8, WorkType.GenerateReports);
+            //performanceHandler(16, WorkType.Negotiation);
+
+            //DoSomething(performanceHandler);
+
+            del += workHandler + performanceHandler;
+            del(12, WorkType.GoToMeetings);
 
             Console.ReadKey();
+        }
+
+        public static void DoSomething(WorkPerformedHandler del)
+        {
+            Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod() + " method called");
+            del(34, WorkType.Golf);
         }
 
         public static void DoSomeWork(int hours, WorkType workType)
