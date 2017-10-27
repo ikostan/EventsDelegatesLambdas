22a812bc1fa898e5ed5ee291852499989be4393a
ikostan <superegorius@gmail.com>
2017-10-26 21:37:40 -0700

    CustomDelegate project added

---
+++ CustomDelegate/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomDelegate
+{
+    public class Program
+    {
+        public delegate void WorkPerformedHandler(int hours, WorkType workType);
+
+        static void Main(string[] args)
+        {
+            WorkPerformedHandler workHandler = new WorkPerformedHandler(WorkPerformed);
+            WorkPerformedHandler performanceHandler = new WorkPerformedHandler(DoSomeWork);
+
+            workHandler(8, WorkType.GenerateReports);
+            performanceHandler(16, WorkType.Negotiation);
+
+            Console.ReadKey();
+        }
+
+        public static void DoSomeWork(int hours, WorkType workType)
+        {
+            Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod() + " method called");
+            Console.WriteLine($"Arguments: {hours}, {workType}");
+        }
+
+        public static void WorkPerformed(int hours, WorkType workType)
+        {
+            Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod() + " method called");
+            Console.WriteLine($"Arguments: {hours}, {workType}");
+        }
+    }
+
+    public enum WorkType
+    {
+        GoToMeetings,
+        Golf,
+        GenerateReports,
+        Negotiation
+    }
+
+}
