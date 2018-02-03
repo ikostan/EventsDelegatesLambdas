7e32be3868e9c87aa1d4200f266b627251f43a95
ikostan <igorkostan@gmail.com>
2018-02-02 18:39:31 -0800

    Anonimus function

--- UsingGenericEventHandler/Timer.cs
+++ UsingGenericEventHandler/Timer.cs
@@ -11,7 +11,13 @@ namespace UsingGenericEventHandler
         public void Subscribe(Clock c)
         {
             //c.timeChanged += new EventHandler<TimeArgs>(ShowTime); //Full reference version
-            c.timeChanged += ShowTime; //Short reference version
+            //c.timeChanged += ShowTime; //Short reference version
+
+            //Anonimus function
+            c.timeChanged += delegate (object src, TimeArgs args)
+            {
+                Console.WriteLine($"{args.Hours} : {args.Minutes} : {args.Seconds}");
+            };
         }
 
         public void ShowTime(object src, TimeArgs args)
